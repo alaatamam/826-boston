eed27a1de39c5730688a40d4e9278a96afb66fdb
robpatterson13 <robertpatterson1313@gmail.com>
2025-10-13 11:00:36 -0400

    removed unnecessary imports from library controller

--- apps/backend/src/library/library.module.ts
+++ apps/backend/src/library/library.module.ts
@@ -3,13 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { LibraryController } from './library.controller';
 import { LibraryService } from './library.service';
 import { Library } from './library.entity';
-import { JwtStrategy } from '../auth/jwt.strategy';
-import { CurrentUserInterceptor } from '../interceptors/current-user.interceptor';
-import { AuthService } from '../auth/auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Library])],
   controllers: [LibraryController],
-  providers: [LibraryService, AuthService, JwtStrategy, CurrentUserInterceptor],
+  providers: [LibraryService],
 })
 export class LibraryModule {}
